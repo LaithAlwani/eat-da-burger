8077bbc2139e4becbb50e991e803b3904baf2d47
Laith Alwani <laithalwani@gmail.com>
2021-02-20 23:58:49 -0500

    added a javascript file in public folder and set the template for index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,11 @@ const burger = require('../models/burger');
 const router = express.Router();
 
 //TODO create all routers
+router.get('/', (req,res)=>{
+    burger.selectAll(function(data){
+        res.render('index', {burgers:data});
+    });
+});
 
 module.exports = router;
 
